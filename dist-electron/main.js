f0a7a0c5c658035bb2977980fea36040a38635ee
MSF MSF <msfmsfyt@gmail.com>
2025-12-26 20:30:20 -0800

    Phase 18.5 - Polish - Lyrics display click skip fix + readme fix

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -2,10 +2,10 @@ import "electron";
 import "node:url";
 import "node:path";
 import "node:fs";
-import { Z, _, Y } from "./main-BWshP3fC.js";
+import { Z as _, _ as i, Y as D } from "./main-CdbUEzzC.js";
 import "node:http";
 export {
-  Z as MAIN_DIST,
-  _ as RENDERER_DIST,
-  Y as VITE_DEV_SERVER_URL
+  _ as MAIN_DIST,
+  i as RENDERER_DIST,
+  D as VITE_DEV_SERVER_URL
 };
