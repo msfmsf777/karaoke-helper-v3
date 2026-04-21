ff50d98f827181c636c06140ddfb98c7e75c3e21
MSF MSF <msfmsfyt@gmail.com>
2026-04-21 12:22:30 -0700

    Phase 22.4.1 - v3.2.0 - Search enhance hori bar

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -66,7 +66,7 @@ const SongList: React.FC<SongListProps> = ({
             {/* Virtualized List */}
             <div style={{ flex: 1 }}>
                 <Virtuoso
-                    style={{ height: '100%', overflowY: 'scroll' }}
+                    style={{ height: '100%', overflowY: 'scroll', overflowX: 'hidden' }}
                     totalCount={songs.length}
                     components={{
                         Footer: () => <div style={{ height: '120px' }} />
